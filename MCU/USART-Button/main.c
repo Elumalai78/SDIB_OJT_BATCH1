912b57bd0195b8ae5db2bc6a2642d9d3ef9e1c3e
Elumalai78 <114151592+Elumalai78@users.noreply.github.com>
2023-03-30 09:28:55 +0530

    Add files via upload

---
+++ MCU/USART-Button/main.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+UART_HandleTypeDef huart2;
+
+uint8_t RX1_Char = 0x00;
+
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART1_UART_Init(void);
+
+//---------[ UART Data Reception Completion CallBackFunc. ]---------
+void HAL_USART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+    HAL_UART_Receive_IT(&huart2, &RX1_Char, 1);
+}
+
+int main(void)
+{
+  uint8_t MSG1[] = "Button State: Released\r\n";
+  uint8_t MSG2[] = "Button State: Pressed\r\n";
+
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+  HAL_UART_Receive_IT(&huart2, &RX1_Char, 1);
+
+  while (1)
+  {
+    //--------[ Read The Button State & Send It Via UART ]---------
+    if(HAL_GPIO_ReadPin (GPIOB, GPIO_PIN_14))
+    {
+      HAL_UART_Transmit(&huart2, MSG2, sizeof(MSG2), 100);
+    }
+    else
+    {
+      HAL_UART_Transmit(&huart2, MSG1, sizeof(MSG1), 100);
+    }
+    //--------[ Read The Received Character & Toggle LEDs Accordingly ]--------
+    if(RX1_Char == '1')
+    {
+          HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_12);
+      HAL_UART_Receive_IT(&huart2, &RX1_Char, 1);
+      RX1_Char = 0x00;
+    }
+    if(RX1_Char == '2')
+    {
+      HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_13);
+      HAL_UART_Receive_IT(&huart2, &RX1_Char, 1);
+      RX1_Char = 0x00;
+    }
+    HAL_Delay(100);
+  }
+}
