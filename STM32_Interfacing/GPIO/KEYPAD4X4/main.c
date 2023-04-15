4e158c9d55b277db707081bf04cd2e16e826b599
Elumalai78 <114151592+Elumalai78@users.noreply.github.com>
2023-04-15 11:10:10 +0530

    Add files via upload

--- STM32_Interfacing/GPIO/KEYPAD4X4/main.c
+++ STM32_Interfacing/GPIO/KEYPAD4X4/main.c
@@ -87,35 +87,35 @@ int main(void)
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
-uint8_t c1,c2,c3;
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
-	        HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, 0);
-	 	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, 0);
-	 	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, 0);
-	 	    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8, 0);
-	 	    HAL_Delay(200);
-
-	 	    c1=HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0);
-	 	    c2=HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0);
-	 	    c3=HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_1);
-
-
-	 	  if(c1!=1 || c2!=1 || c3!=1)
-	 	  {
-	 		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 1);
-
-	 	  }
-
-	 	  else{
-	 		  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, 0);
-	 	  }
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+  uint8_t c1,c2,c3,c4;
+    uint8_t n=0;
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1)
+    {
+  	  	 	   HAL_GPIO_WritePin(GPIOA, GPIO_PIN_3, 0);
+  	  	 	  	 	    HAL_Delay(200);
+  	  	 	    c1=HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_0);
+  	  	 	    c2=HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_0);
+  	  	 	    c3=HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_1);
+  	  	 	    c4=HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_4);
+  	  	 	    if(c1==0)
+  					n=1;
+  	  	 	    else if(c2==0)
+  					n=2;
+  	  	 	    else if(c3==0)
+  					n=3;
+  	  	 	    else if(c4==0)
+  	  	 	    	n=4;
+  	  	 	    for( uint8_t i=1;i<=n;i++)
+  	  	 	    {
+  	  	 	    	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_2, 1);
+  	  	 	    	HAL_Delay(200);
+  	  	 	    	HAL_GPIO_WritePin(GPIOA,GPIO_PIN_2, 0);
+  	  	 	    	HAL_Delay(200);
+  	  	 	    }
+    }
+    /* USER CODE END 3 */
 }
 
 /**
@@ -175,10 +175,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_2|GPIO_PIN_3, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PC0 PC1 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
@@ -186,24 +183,23 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA5 PA6 PA7 PA8 */
-  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7|GPIO_PIN_8;
+  /*Configure GPIO pins : PA2 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PA4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PB0 */
   GPIO_InitStruct.Pin = GPIO_PIN_0;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-  /*Configure GPIO pin : PB6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_6;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
